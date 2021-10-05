1207b9fc10babed9bd0ccc255e6fca28eef8024f
zishan@0509 <zishanali20071@gmail.com>
2021-10-05 19:03:29 +0530

    Here i have done how to remove and add element in array

---
+++ array__pop__push.js
@@ -0,0 +1,6 @@
+let a =[44,55,22,11,22,33,4,6]
+console.log(a.pop())
+console.log(a.push(2))
+
+// pop method is using to remove lastelement of array
+//push method is using to add new element in last of array but it count length of array 
