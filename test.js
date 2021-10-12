678c07a77e6b29648a4e476d19ce1aca633519a0
shaukat1206 <shaukatali102007@gmail.com>
2021-10-12 14:31:31 +0530

    abhi bheja hu

--- test.js
+++ test.js
@@ -1,9 +1,16 @@
-function digital_root(n){
-    let a =0
-    for (let i =1 ; i <=n.length;i++){
-        a+=n[i]
-    }
-    console.log(a)
-}
-digital_root(19)
-console.log("nowddddhhhr")
+// function digital_root(n){
+//     let a =0
+//     for (let i =1 ; i <=n.length;i++){
+//         a+=n[i]
+//     }
+//     console.log(a)
+// }
+// digital_root(19)
+// console.log("nowddddfffhhhr")
+let arr=[34,56,23,45]
+
+let res = arr.reduce(function(acc, curr){
+    acc=acc+curr
+    return curr
+},0)
+console.log(res)
